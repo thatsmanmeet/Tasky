bb090d5bf6cc90944d103aac3da8e3baaf779e45
Manmeet Singh <120583077+thatsmanmeet@users.noreply.github.com>
2023-04-11 17:28:33 +0530

    Added code for restarting reminders for tasks after device has been rebooted

--- app/src/main/java/com/thatsmanmeet/taskyapp/screens/SettingsScreen.kt
+++ app/src/main/java/com/thatsmanmeet/taskyapp/screens/SettingsScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
